6f48fd4e55f8bb16704f444486d95c86ae6f0d82
Marcelo Surco <marcelo.surco@utec.edu.pe>
2024-03-09 16:21:22 -0500

    add auth to fastapi

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI, Request
-from pydantic import BaseModel, Field, validator
+from fastapi import FastAPI, Request, HTTPException, Depends, Response, Cookie
+from pydantic import BaseModel, Field, validator, EmailStr
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.responses import JSONResponse
 import re
+import secrets
+import json
 
 app = FastAPI()
 
+USERNAME = "admin"
+PASSWORD = "password"
+SESSION_COOKIE_NAME = "session"
 
+session_db = {}
 class User(BaseModel):
     id : int = None
     username : str = Field(..., min_length=3, max_length=50)
-    email : str = Field(..., pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+")
+    email : EmailStr
     image_file : str = None
     name : str = None
     password : str = None
@@ -26,7 +34,28 @@ class User(BaseModel):
             raise ValueError('debe contener unicamente letras y espacios')
         return v
 
+class SessionData(BaseModel):
+    username: str
 
+
+def generate_session_token():
+    return secrets.token_hex(16)
+
+def create_session(username: str):
+    session_token = generate_session_token()
+    session_db[session_token] = SessionData(username=username)
+    return session_token
+
+def get_session(session_token: str):
+    print(session_db)
+    print(session_token)
+    return session_db.get(session_token)
+
+def get_current_session(session_token: str = Cookie(None)):
+    session_data = get_session(session_token)
+    if session_data is None:
+        raise HTTPException(status_code=401, detail="Invalid session")
+    return session_data
 
 @app.get("/")
 def read_root():
@@ -36,6 +65,39 @@ def read_root():
 @app.post("/user")
 async def create_user(user: User):
     return user
+
+
+def check_auth(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    correct_username = credentials.username == USERNAME
+    correct_password = credentials.password == PASSWORD
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return True
+
+
+
+@app.post("/login")
+async def login(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    if check_auth(credentials):
+        # Generamos un session para el usuario
+        session_token = create_session(credentials.username)
+        response_content= {"message": "Login successful"}
+        response = Response(content=json.dumps(response_content))
+        response.set_cookie(key = SESSION_COOKIE_NAME, value=session_token)
+        return response
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized",
+        )
+
+@app.get("/secure")
+async def secure_endpoint(session_data: SessionData = Depends(get_current_session)):
+    return JSONResponse(content={"message": "Secure content"})
 
 
 if __name__ == "__main__":
